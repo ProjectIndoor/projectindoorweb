68b13b1505af96b91ca816ad6926f472ff9756bd
Dobli <61doal1mst@hft-stuttgart.de>
2017-10-26 15:50:35 +0200

    Use less strict sun style checks

--- src/main/java/de/hft_stuttgart/projectindoorweb/Application.java
+++ src/main/java/de/hft_stuttgart/projectindoorweb/Application.java
@@ -1,8 +1,5 @@
 package de.hft_stuttgart.projectindoorweb;
 
-import de.hft_stuttgart.projectindoorweb.de.hft_stuttgart.projectindoorweb.parser.SensorFileReader;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //@SpringBootApplication
 public class Application {
