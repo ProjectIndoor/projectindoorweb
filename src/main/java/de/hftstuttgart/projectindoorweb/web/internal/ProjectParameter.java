aaec623a1ba95d83241d85dcbebb0cf02ac10406
VolkanKutlar <volkan.kutlar@gmx.de>
2017-11-25 19:57:04 +0100

    Added missing Json annotations to REST result objects

--- src/main/java/de/hftstuttgart/projectindoorweb/web/internal/ProjectParameter.java
+++ src/main/java/de/hftstuttgart/projectindoorweb/web/internal/ProjectParameter.java
@@ -1,13 +1,15 @@
 package de.hftstuttgart.projectindoorweb.web.internal;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class ProjectParameter {
 
     private  String name;
     private  String value;
 
-    public ProjectParameter(){}
-
-    public ProjectParameter(String name, String value) {
+    @JsonCreator
+    public ProjectParameter(@JsonProperty("name")String name, @JsonProperty("value")String value) {
         this.name = name;
         this.value = value;
     }
