f511e92a4f4a502ff1b4cfe9357d967571dcf95b
geraldmaurer <gerald.maurer@gmail.com>
2017-12-15 18:52:36 +0100

    Adjust the POSILogMerger and include JavaDoc

--- src/main/java/de/hftstuttgart/projectindoorweb/geoCalculator/transformation/POSILogMerger.java
+++ src/main/java/de/hftstuttgart/projectindoorweb/geoCalculator/transformation/POSILogMerger.java
@@ -6,22 +6,29 @@ import de.hftstuttgart.projectindoorweb.geoCalculator.internal.LocalXYCoord;
 import de.hftstuttgart.projectindoorweb.geoCalculator.internal.XYPoint;
 import de.hftstuttgart.projectindoorweb.persistence.entities.Building;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * Class that merges an Evaal-Logilfe with empty POSI-entries with a given list of reference points.
+ */
 public class POSILogMerger {
 
-    private static List readValuesFromFile(String filePath) {
+    /**
+     * Reads the file that contains the reference-points in pixel-coordinates.
+     *
+     * @param file File that contains the reference-points
+     * @return Point-list with all reference-points
+     */
+    private static List readValuesFromFile(final File file) {
         List parsedDataList = new ArrayList<>();
         try {
-            Files.readAllLines(Paths.get(filePath)).stream().filter(s -> {
-
+            Files.readAllLines(file.toPath()).stream().filter(s -> {
                 return isValidLine(s);
-
             }).map(s -> {
 
                 String[] elements = s.split(";");
@@ -31,7 +38,7 @@ public class POSILogMerger {
                 double building = Double.valueOf(elements[3]);
                 double floor = Double.valueOf(elements[4]);
 
-                return new XYPoint(pointNumber, new LocalXYCoord(coord1,coord2), building, floor);
+                return new XYPoint(pointNumber, new LocalXYCoord(coord1, coord2), building, floor);
 
             }).collect(Collectors.toCollection(() -> parsedDataList));
             return parsedDataList;
@@ -41,41 +48,56 @@ public class POSILogMerger {
         }
     }
 
-    private static boolean isValidLine(String line) {
+    /**
+     * Checks if the line is valid.
+     *
+     * @param line Line to check
+     * @return returns true if the line is correct
+     */
+    private static boolean isValidLine(final String line) {
         return !line.contains("%")
                 && !line.isEmpty()
                 && line.split(";").length == 5;
     }
 
-    public static List<String> combineReferencePointsWithPOSILines(List<String> POSILines, String referenceFile, Building b, int width, int height)
-    {
+    /**
+     * Merges the POSI-Lines with the given reference-point information.
+     *
+     * @param posiLines     all POSI-Lines of one EVaal-File
+     * @param referenceFile File with the pixel-coordinates of the reference points
+     * @param b             Building-object, that contains the coordinates of the corners of the image/picture
+     * @param width         width of the picture
+     * @param height        height of the picture
+     * @return List with all POSI-Lines with filled in coordinates, floor and building informations
+     */
+    public static List<String> combineReferencePointsWithPOSILines(final List<String> posiLines, final File referenceFile, final Building b, final int width, final int height) {
 
-        List<XYPoint> Points = readValuesFromFile(referenceFile);
-        List<LLPoint> transformedPoints =  new ArrayList<LLPoint>();
-        for (int j = 0; j < Points.size(); j++) {
-            double[] wgs = TransformationHelper.pictToWGS(b,Points.get(j).getCoords(),width,height);
-            transformedPoints.add(new LLPoint(Points.get(j).getPointNumber(),new LatLongCoord(wgs[0],wgs[1]),Points.get(j).getBuilding(),Points.get(j).getFloor()));
+        List<XYPoint> points = readValuesFromFile(referenceFile);
+        List<LLPoint> transformedPoints = new ArrayList<LLPoint>();
+        for (int j = 0; j < points.size(); j++) {
+            double[] wgs = TransformationHelper.pictToWGS(b, points.get(j).getCoords(), width, height);
+            transformedPoints.add(new LLPoint(points.get(j).getPointNumber(), new LatLongCoord(wgs[0], wgs[1]), points.get(j).getBuilding(), points.get(j).getFloor()));
         }
 
-        if (POSILines.size() != transformedPoints.size()) {
+        if (posiLines.size() != transformedPoints.size()) {
             return null;
         }
 
         List<String> result = new ArrayList<String>();
-        for (int i = 0; i< POSILines.size(); i++) {
-            String[] line = POSILines.get(i).split(";");
-            if (line[0].equals("POSI")&& line.length == 7) {
+        for (int i = 0; i < posiLines.size(); i++) {
+            String[] line = posiLines.get(i).split(";");
+            if (line[0].equals("POSI") && line.length == 7) {
                 LLPoint p = (LLPoint) transformedPoints.get(i);
                 line[3] = Double.toString(p.getCoords().latitude);
                 line[4] = Double.toString(p.getCoords().longitude);
                 line[5] = Double.toString(p.getFloor());
                 line[6] = Double.toString(p.getBuilding());
 
-                result.add(line[0]+";"+line[1]+";"+line[2]+";"+line[3]+";"+line[4]+";"+line[5]+";"+line[6]+";");
-            }else {
-                result.add(POSILines.get(i));
+                result.add(line[0] + ";" + line[1] + ";" + line[2] + ";" + line[3] + ";" + line[4] + ";" + line[5] + ";" + line[6] + ";");
+            } else {
+                result.add(posiLines.get(i));
             }
         }
-        return  result;
+        return result;
     }
 }
