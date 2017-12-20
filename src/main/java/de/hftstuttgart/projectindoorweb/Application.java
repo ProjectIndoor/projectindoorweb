eda912c7137e92d09bd1b1e9e1753e92014e5b9c
dobli <dobler.alex@gmail.com>
2017-12-21 00:08:35 +0100

    Moved Application.java to project package root to enable auto configuration

--- src/main/java/de/hftstuttgart/projectindoorweb/Application.java
+++ src/main/java/de/hftstuttgart/projectindoorweb/Application.java
@@ -1,4 +1,4 @@
-package de.hftstuttgart.projectindoorweb.application;
+package de.hftstuttgart.projectindoorweb;
 
 
 import com.fasterxml.classmate.TypeResolver;
@@ -6,21 +6,13 @@ import de.hftstuttgart.projectindoorweb.application.internal.util.IndoorApiDescr
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger.web.UiConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication(scanBasePackages = {
-        "de.hftstuttgart.projectindoorweb.persistence",
-        "de.hftstuttgart.projectindoorweb.positionCalculator",
-        "de.hftstuttgart.projectindoorweb.inputHandler",
-        "de.hftstuttgart.projectindoorweb.web"})
+@SpringBootApplication
 @EnableSwagger2
-@EntityScan("de.hftstuttgart.projectindoorweb.persistence.entities")
-@EnableJpaRepositories("de.hftstuttgart.projectindoorweb.persistence.repositories")
 public class Application {
 
     @Autowired
