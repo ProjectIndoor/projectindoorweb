78acc976442ab637bfdc699be87dd2dfe31a8617
VolkanKutlar <volkan.kutlar@gmx.de>
2017-12-25 12:17:21 +0100

    Implement mechanism to give back detailed warning and error information to frontend #42 Preparations for type long result messages

---
+++ src/main/java/de/hftstuttgart/projectindoorweb/web/internal/ResponseWrapper.java
@@ -0,0 +1,28 @@
+package de.hftstuttgart.projectindoorweb.web.internal;
+
+public class ResponseWrapper {
+
+    private long id;
+    private String message;
+
+    public ResponseWrapper(long id, String message) {
+        this.id = id;
+        this.message = message;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
